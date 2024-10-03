8957d8020ca1c00d4f015f5bc618fc6b4a24fa3c
natanaelcloud <natanpaivacosta@gmail.com>
2024-10-02 21:58:00 -0300

    adicionar let de nome split

--- index.js
+++ index.js
@@ -2,4 +2,5 @@
 function update(){
     let bill = Num[document.getElementById('yourBill').value]
     let tipPercent = document.getElementById('tipInput').value    
+    let split = document.getElementById('splitInput')
 }
